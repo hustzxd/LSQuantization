ddf823d63298e86a0a5bf80666619020f65aa5f2
2227820226@qq.com <2227820226@qq.com>
2020-07-17 11:22:28 +0800

    alexnet result :chart_with_upwards_trend:

--- _quan_base.py
+++ _quan_base.py
@@ -16,60 +16,69 @@ class Qmodes(Enum):
 
 class _Conv2dQ(nn.Conv2d):
     def __init__(self, in_channels, out_channels, kernel_size, stride=1,
-                 padding=0, dilation=1, groups=1, bias=True, nbits=4,
-                 mode=Qmodes.kernel_wise):
+                 padding=0, dilation=1, groups=1, bias=True, **kwargs_q):
         super(_Conv2dQ, self).__init__(in_channels, out_channels, kernel_size, stride=stride,
                                        padding=padding, dilation=dilation, groups=groups, bias=bias)
-        if nbits < 0:
+        self.kwargs_q = get_default_kwargs_q(kwargs_q, layer_type=self)
+        self.nbits = kwargs_q['nbits']
+        if self.nbits < 0:
             self.register_parameter('alpha', None)
             return
-        self.nbits = nbits
-        self.q_mode = mode
-        if mode == Qmodes.kernel_wise:
+        self.q_mode = kwargs_q['mode']
+        if self.q_mode == Qmodes.kernel_wise:
             self.alpha = Parameter(torch.Tensor(out_channels))
-            self.is_layer_wise = False
         else:  # layer-wise quantization
             self.alpha = Parameter(torch.Tensor(1))
-            self.is_layer_wise = True
         self.register_buffer('init_state', torch.zeros(1))
+
+    def add_param(self, param_k, param_v):
+        self.kwargs_q[param_k] = param_v
 
     def extra_repr(self):
         s_prefix = super(_Conv2dQ, self).extra_repr()
         if self.alpha is None:
             return '{}, fake'.format(s_prefix)
-        return '{}, nbits={}, qmode={}'.format(s_prefix, self.nbits, self.q_mode, )
-
+        return '{}, {}'.format(s_prefix, self.kwargs_q)
 
 class _LinearQ(nn.Linear):
-    def __init__(self, in_features, out_features, bias=True, nbits=4):
+    def __init__(self, in_features, out_features, bias=True, **kwargs_q):
         super(_LinearQ, self).__init__(in_features=in_features, out_features=out_features, bias=bias)
-        if nbits < 0:
+        self.kwargs_q = get_default_kwargs_q(kwargs_q, layer_type=self)
+        self.nbits = kwargs_q['nbits']
+        if self.nbits < 0:
             self.register_parameter('alpha', None)
             return
-        self.nbits = nbits
         self.alpha = Parameter(torch.Tensor(1))
         self.register_buffer('init_state', torch.zeros(1))
+
+    def add_param(self, param_k, param_v):
+        self.kwargs_q[param_k] = param_v
 
     def extra_repr(self):
         s_prefix = super(_LinearQ, self).extra_repr()
         if self.alpha is None:
             return '{}, fake'.format(s_prefix)
-        return '{}, nbits={}'.format(s_prefix, self.nbits)
+        return '{}, {}'.format(s_prefix, self.kwargs_q)
+
 
 
 class _ActQ(nn.Module):
-    def __init__(self, nbits=4, sign=False):
+    def __init__(self, **kwargs_q):
         super(_ActQ, self).__init__()
-        if nbits < 0:
+        self.kwargs_q = get_default_kwargs_q(kwargs_q, layer_type=self)
+        self.nbits = kwargs_q['nbits']
+        if self.nbits < 0:
             self.register_parameter('alpha', None)
             return
-        self.nbits = nbits
-        self.sign = sign
+        self.signed = kwargs_q['signed']
         self.alpha = Parameter(torch.Tensor(1))
         self.register_buffer('init_state', torch.zeros(1))
 
+    def add_param(self, param_k, param_v):
+        self.kwargs_q[param_k] = param_v
+
     def extra_repr(self):
-        s_prefix = super(_ActQ, self).extra_repr()
+        # s_prefix = super(_ActQ, self).extra_repr()
         if self.alpha is None:
-            return '{}, fake'.format(s_prefix)
-        return '{}, nbits={}, sign={}'.format(s_prefix, self.nbits, self.sign)
+            return 'fake'
+        return '{}'.format(self.kwargs_q)
--- lsq.py
+++ lsq.py
@@ -1,6 +1,12 @@
 """
-    LSQ: Learned Step Size Quantization
-    ICLR2020 open review
+@inproceedings{
+    esser2020learned,
+    title={LEARNED STEP SIZE QUANTIZATION},
+    author={Steven K. Esser and Jeffrey L. McKinstry and Deepika Bablani and Rathinakumar Appuswamy and Dharmendra S. Modha},
+    booktitle={International Conference on Learning Representations},
+    year={2020},
+    url={https://openreview.net/forum?id=rkgO66VKDS}
+}
     https://quanoview.readthedocs.io/en/latest/_raw/LSQ.html
 """
 import torch
@@ -16,6 +22,7 @@ __all__ = ['Conv2dLSQ', 'LinearLSQ', 'ActLSQ']
 class FunLSQ(torch.autograd.Function):
     @staticmethod
     def forward(ctx, weight, alpha, g, Qn, Qp):
+        assert alpha > 0, 'alpha = {}'.format(alpha)
         ctx.save_for_backward(weight, alpha)
         ctx.other = g, Qn, Qp
         q_w = (weight / alpha).round().clamp(Qn, Qp)
@@ -33,9 +40,6 @@ class FunLSQ(torch.autograd.Function):
         grad_alpha = ((indicate_small * Qn + indicate_big * Qp + indicate_middle * (
                 -q_w + q_w.round())) * grad_weight * g).sum().unsqueeze(dim=0)
         grad_weight = indicate_middle * grad_weight
-        # The following operation can make sure that alpha is always greater than zero in any case and can also
-        # suppress the update speed of alpha. (Personal understanding)
-        grad_alpha.clamp_(-alpha.item(), alpha.item())  # FYI
         return grad_weight, grad_alpha, None, None, None
 
 
@@ -61,71 +65,73 @@ class Conv2dLSQ(_Conv2dQ):
             nbits=nbits, mode=mode)
 
     def forward(self, x):
-        if self.nbits < 0:
+        if self.alpha is None:
             return F.conv2d(x, self.weight, self.bias, self.stride,
                             self.padding, self.dilation, self.groups)
-        # w_reshape = self.weight.reshape([self.weight.shape[0], -1]).transpose(0, 1)
-        if self.training and self.init_state == 0:
-            self.alpha.data.copy_(self.weight.abs().max() / 2 ** (self.nbits - 1))
-
-            # self.alpha.data.copy_(self.weight.abs().max() * 2)
-            self.init_state.fill_(1)
-        
-
-        ## Pslease see STE_LSQ.ipynb for detailed comparison.
-        """ Method1: 
-        g = 1.0 / math.sqrt(self.weight.numel() * Qp)
-        alpha = grad_scale(self.alpha, g)
-        w = self.weight / alpha
-        w = w.clamp(-(2 ** (self.nbits - 1)), (2 ** (self.nbits - 1) - 1))
-        q_w = round_pass(w)
-        w_q = q_w * alpha
-        """
-
-        # Method2:
         Qn = -2 ** (self.nbits - 1)
         Qp = 2 ** (self.nbits - 1) - 1
+        if self.training and self.init_state == 0:
+            self.alpha.data.copy_(2 * self.weight.abs().mean() / math.sqrt(Qp))
+            self.init_state.fill_(1)
+        
         g = 1.0 / math.sqrt(self.weight.numel() * Qp)
-        w_q = FunLSQ.apply(self.weight, self.alpha, g, Qn, Qp)
-        # wq = y.transpose(0, 1).reshape(self.weight.shape).detach() + self.weight - self.weight.detach()
+
+        # Method1: 31GB GPU memory (AlexNet w4a4 bs 2048) 17min/epoch
+        alpha = grad_scale(self.alpha, g)
+        w_q = round_pass((self.weight / alpha).clamp(Qn, Qp)) * alpha
+
+        # Method2: 25GB GPU memory (AlexNet w4a4 bs 2048) 32min/epoch
+        # w_q = FunLSQ.apply(self.weight, self.alpha, g, Qn, Qp)
         return F.conv2d(x, w_q, self.bias, self.stride,
                         self.padding, self.dilation, self.groups)
 
 
 class LinearLSQ(_LinearQ):
     def __init__(self, in_features, out_features, bias=True, nbits=4):
-        super(_LinearQ, self).__init__(in_features=in_features, out_features=out_features, bias=bias, nbits=nbits)
+        super(LinearLSQ, self).__init__(in_features=in_features, out_features=out_features, bias=bias, nbits=nbits)
 
     def forward(self, x):
         if self.alpha is None:
             return F.linear(x, self.weight, self.bias)
-        if self.training and self.init_state == 0:
-            self.alpha.data.copy_(self.weight.abs().max() / 2 ** (self.nbits - 1))
-            self.init_state.fill_(1)
         Qn = -2 ** (self.nbits - 1)
         Qp = 2 ** (self.nbits - 1) - 1
+        if self.training and self.init_state == 0:
+            self.alpha.data.copy_(2 * self.weight.abs().mean() / math.sqrt(Qp))
+            self.init_state.fill_(1)
         g = 1.0 / math.sqrt(self.weight.numel() * Qp)
-        w_q = FunLSQ.apply(self.weight, self.alpha, g, Qn, Qp)
-        return F.conv2d(input, w_q, self.bias, self.stride,
-                        self.padding, self.dilation, self.groups)
+
+        # Method1:
+        alpha = grad_scale(self.alpha, g)
+        w_q = round_pass((self.weight / alpha).clamp(Qn, Qp)) * alpha
+
+        # Method2:
+        # w_q = FunLSQ.apply(self.weight, self.alpha, g, Qn, Qp)
+        return F.linear(x, w_q, self.bias)
 
 
 class ActLSQ(_ActQ):
-    def __init(self, nbits=4, sign=False):
-        super(ActLSQ, self).__init(nbits=nbits, sign=sign)
+    def __init(self, nbits=4, signed=False):
+        super(ActLSQ, self).__init(nbits=nbits, signed=signed)
 
     def forward(self, x):
         if self.alpha is None:
             return x
-        if self.training and self.init_state == 0:
-            self.alpha.data.copy_(x.max() / 2 ** (self.nbits - 1) * 0.25)
-            self.init_state.fill_(1)
-        if self.sign:
+        if self.signed:
             Qn = -2 ** (self.nbits - 1)
             Qp = 2 ** (self.nbits - 1) - 1
         else:
             Qn = 0
             Qp = 2 ** self.nbits - 1
+        if self.training and self.init_state == 0:
+            self.alpha.data.copy_(2 * x.abs().mean() / math.sqrt(Qp))
+            self.init_state.fill_(1)
+
         g = 1.0 / math.sqrt(x.numel() * Qp)
-        x_q = FunLSQ.apply(x, self.alpha, g, Qn, Qp)
-        return x_q
+
+        # Method1:
+        alpha = grad_scale(self.alpha, g)
+        x = round_pass((x / alpha).clamp(Qn, Qp)) * alpha
+
+        # Method2:
+        # x_q = FunLSQ.apply(x, self.alpha, g, Qn, Qp)
+        return x
